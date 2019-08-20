61ed478ba56b9aaed95fd0261fbb5b0871179177
Jonathan Calderaro <jcc2019.lambda@gmail.com>
2019-08-20 15:08:21 -0700

    Default

--- assignments/closure.js
+++ assignments/closure.js
@@ -2,7 +2,7 @@
 // Write a simple closure of your own creation.  Keep it simple!
 
 function battleCry() {
-  let name = "Godzilla";
+  let name = "Godzilla!!!";
   function rawr() {
   console.log("Watch out it's " + name)
 }
