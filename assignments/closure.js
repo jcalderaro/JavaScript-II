badeb50e4da2560e5da2a2aebc0b281c7144d1a6
Jonathan Calderaro <jcc2019.lambda@gmail.com>
2019-08-20 15:24:37 -0700

    MVP 2

--- assignments/closure.js
+++ assignments/closure.js
@@ -4,6 +4,14 @@
 // that manipulates variables defined in the outer scope.
 // The outer scope can be a parent function, or the top level of the script.
 
+function battleCry() {
+  let name = "Godzilla";
+  function rawr() {
+  console.log("Watch out it's " + name)
+}
+rawr();
+}
+battleCry();
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
